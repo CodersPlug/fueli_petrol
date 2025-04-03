fe69da7773044f0989147e38ffdbd641e3f352af
codersplug <leo@codersplug.com>
2025-04-03 18:38:01 -0300

    Add credentials.py

---
+++ credentials.py
@@ -0,0 +1,12 @@
+"""Credentials configuration for the application"""
+
+USERS = {
+    "admin1": {
+        "password": "admin123",
+        "organization": "Petrol"
+    },
+    "admin2": {
+        "password": "admin123",
+        "organization": "Posta"
+    }
+} 
